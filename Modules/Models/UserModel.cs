a6d2053d9ff9d86f7a932c23ee3f6f6d63782540
RehamAfzal <reham.afzal1@gmail.com>
2024-12-03 23:08:32 -0600

    Update UserModel.cs

--- Modules/Models/UserModel.cs
+++ Modules/Models/UserModel.cs
@@ -1,15 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+/*
+ * Program Author: Reham Afzal  
+ * ID: w10171356   
+ * Assignment: Password Manager part 2
+ */
 
 namespace CSC317PassManagerP2Starter.Modules.Models
 {
     public class User
     {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] Key { get; set; }
+        public byte[] IV { get; set; }
 
-        //Implement the User Model here.
-
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}, Username: {UserName}";
+        }
     }
 }
